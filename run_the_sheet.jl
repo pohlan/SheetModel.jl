dda9290ae0163300e3f4270bcbc6147763d0ef13
Annegret Pohle <apohle@ethz.ch>
2021-06-03 18:29:11 +0200

    Params struct and defined small functions

--- run_the_sheet.jl
+++ run_the_sheet.jl
@@ -1,4 +1,4 @@
-using Pkg, PyPlot, Printf, LinearAlgebra
+using Pkg, PyPlot, Printf, LinearAlgebra, Parameters
 Pkg.activate(@__DIR__)
 using SheetModel
 S = SheetModel
@@ -10,62 +10,19 @@ pygui(true)
 
 @views function runthemodel(; do_visu=true)
 
-    # Numerical domain
-    lx, ly     = 10.0, 12.0       # domain size
-    nx, ny     = 64, 32        # numerical grid resolution
-    dx     = lx/nx      # grid size
-    dy     = ly/ny
-    xc     = LinRange(dx/2, lx-dx/2, nx)
-    yc     = LinRange(dy/2, ly-dy/2, ny)
 
-    # Dimensionless numbers
-    sigma = 0
-    gamma = 0
-    lambda = 0
-    r_rho = 1000 / 910 # rhow / rhoi
-
-    # Physical parameters
-    k = 1
-    n = 3
-    A = 1
-    g = 1
-    rhow = 1
-    rhoi = rhow / r_rho
-    ev = 0.5
-    lr = 1 # horizontal cavity spacing, spatially uniform
-    hr = 1 # bedrock bump height, spatially uniform
-
-    zs = 1/lx * xc * ones(ny)' # surface elevation
-    zb = zeros(nx, ny) # bed elevation
-    m = zeros(nx-2, ny-2) # source term
-    ub = zeros(nx, ny) # basal sliding speed
-
-    # Time stepping
-    ttot   = 2.0        # total simulation time
-    dt     = 0.1        # physical time step
-
-    # Pseudo-time iteration
-    tol    = 1e-6       # tolerance
-    itMax  = 1e3        # max number of iterations
-    damp   = 1-41/nx    # damping (this is a tuning parameter, dependent on e.g. grid resolution)
-    dtau   = (1.0/(dx^2/k/2.1) + 1.0/dt)^-1 # iterative "timestep"
+    Params = Para()
+    @unpack ev, g, ρw, Σ, Γ, Λ, m, lx, ly, nx, ny, dx, dy, xc, yc, dt, ttot, tol, itMax, damp, dτ = Params
 
     # Array allocation
-    qx     = zeros(nx-1, ny-1)
-    qy     = zeros(nx-1, ny-1)
-
-    dphi_dtau = zeros(nx-2, ny-2)
-    Res_phi   = zeros(nx-2, ny-2)
-
-    dh_dtau = zeros(nx-2, ny-2)
-    Res_h     = zeros(nx-2, ny-2)
+    qx, qy, dϕ_dτ, dh_dτ, Res_ϕ, Res_h = S.array_allocation(Params)
 
     # Initial condition
-    # phi0    = exp.(- 1e-2*(xc.-lx/2).^2) * exp.(-1e-2*(yc.-ly/2).^2)'
-    # phi0 = 1/lx * xc * ones(ny)'
-    phi0 = rand(nx, ny)
-    phi_old   = copy(phi0)
-    phi      = copy(phi0)
+    #ϕ0    = exp.(- 1e-2*(xc.-lx/2).^2) * exp.(-1e-2*(yc.-ly/2).^2)'
+     ϕ0 = 1/lx * xc * ones(ny)'
+    # ϕ0 = rand(nx, ny)
+    ϕ_old   = copy(ϕ0)
+    ϕ      = copy(ϕ0)
 
     h0 = 0.5 * ones(nx, ny)
     h_old = copy(h0)
@@ -75,51 +32,49 @@ pygui(true)
 
     # Physical time loop
     while t<ttot
-        iter = 0; err_phi = 2*tol; err_h = 2*tol;
+        iter = 0; err_ϕ = 2*tol; err_h = 2*tol;
 
         # Pseudo-transient iteration
-        while max(err_phi, err_h) >tol && iter<itMax
-            dphi_dx, dphi_dy = diff(phi, dims=1) ./ dx, diff(phi, dims=2) ./ dy   # hydraulic gradient
-            qx, qy     = S.calc_q(h[1:end-1, :], dphi_dx, k), S.calc_q(h[:, 1:end-1], dphi_dy, k) # TODO: which h values to take?!
+        while max(err_ϕ, err_h) >tol && iter<itMax
+            dϕ_dx, dϕ_dy = diff(ϕ, dims=1) ./ dx, diff(ϕ, dims=2) ./ dy   # hydraulic gradient
+            qx, qy     = S.calc_q(h[1:end-1, :], dϕ_dx, Params), S.calc_q(h[:, 1:end-1], dϕ_dy, Params) # TODO: which h values to take?!
 
-            pw     = phi .- rhow .* g .* zb                   # water pressure
-            N      = rhoi * g * (zs .- zb) .- pw            # effective pressure
-            vo     = ub .* (hr .- h) ./ lr                  # opening rate
-            vc     = 2 / n^n * A * h .* abs.(N).^(n-1) .* N  # closure rate
+            vo     = S.calc_vo(h, Params)                   # opening rate
+            vc     = S.calc_vc(ϕ, h, Params)  # closure rate
 
             # calculate residuals
-            Res_phi        =    - ev/(rhow*g) * (phi[2:end-1, 2:end-1] .- phi_old[2:end-1, 2:end-1])/dt .-    # dhe/dt
+            Res_ϕ        =    - ev/(ρw*g) * (ϕ[2:end-1, 2:end-1] .- ϕ_old[2:end-1, 2:end-1])/dt .-    # dhe/dt
                                 (diff(qx, dims=1)[:, 2:end-1]/dx .+ diff(qy, dims=2)[2:end-1, :]/dy)    .+    # div(q)
-                                sigma * vo[2:end-1, 2:end-1] .- gamma * vc[2:end-1, 2:end-1]            .-    # dh/dt
-                                lambda * m                                                                    # source term
+                                Σ * vo[2:end-1, 2:end-1] .- Γ * vc[2:end-1, 2:end-1]            .-    # dh/dt
+                                Λ * m                                                                    # source term
             Res_h          =    (h[2:end-1, 2:end-1] .- h_old[2:end-1, 2:end-1]) / dt  .-
-                                sigma * vo[2:end-1, 2:end-1] .+ gamma * vc[2:end-1, 2:end-1]
+                                Σ * vo[2:end-1, 2:end-1] .+ Γ * vc[2:end-1, 2:end-1]
 
             # damped rate of change
-            dphi_dtau      = Res_phi .+ damp * dphi_dtau
-            dh_dtau        = Res_h .+ damp * dh_dtau
+            dϕ_dτ      = Res_ϕ .+ damp * dϕ_dτ
+            dh_dτ        = Res_h .+ damp * dh_dτ
 
             # update fields
-            phi[2:end-1, 2:end-1]  = phi[2:end-1, 2:end-1] + dtau * dphi_dtau   # update rule, sets the BC as phi[1]=phi[end]=0
-            h[2:end-1, 2:end-1]    = h[2:end-1, 2:end-1] + dtau * dh_dtau
+            ϕ[2:end-1, 2:end-1]  = ϕ[2:end-1, 2:end-1] + dτ * dϕ_dτ   # update rule, sets the BC as ϕ[1]=ϕ[end]=0
+            h[2:end-1, 2:end-1]    = h[2:end-1, 2:end-1] + dτ * dh_dτ
 
-            err_phi = norm(Res_phi)/length(Res_phi)
+            err_ϕ = norm(Res_ϕ)/length(Res_ϕ)
             err_h   = norm(Res_h)/length(Res_h)
             iter += 1
         end
         ittot += iter; it += 1; t += dt
 
-        phi_old = phi
-        h_old = h
+        ϕ_old .= ϕ
+        h_old .= h
     end
 
     # Visualize
     if do_visu
         figure()
         subplot(1, 2, 1)
-        pcolor(xc, yc, phi0')
+        pcolor(xc, yc, ϕ0')
         subplot(1, 2, 2)
-        pcolor(xc, yc, phi')
+        pcolor(xc, yc, ϕ')
     end
 
 end
--- src/SheetModel.jl
+++ src/SheetModel.jl
@@ -1,10 +1,118 @@
 module SheetModel
 
-alpha = 1.25
-beta = 1.5
-function calc_q(h, dphi_du, k) # u can be x or y
-    q = - k .* h.^alpha .* abs.(dphi_du).^(beta-2) .* dphi_du
+using Base: Float64
+using Parameters
+
+export Para
+
+@with_kw struct Para{F<:Float64, I<:Int64, Arr<:Array{Float64}, Lin<:LinRange{Float64}}
+    # Scalars (one global value)
+    g::F     = 1.0              # gravitational acceleration
+    ρw::F    = 1.0              # water density
+    ρi::F    = 910.0 / 1000.0
+    α::F     = 1.25
+    β::F     = 1.5
+    k::F     = 1.0
+    n::F     = 3.0
+    A::F     = 1.0
+    ev::F    = 0.5
+    lr::F    = 1.0              # horizontal cavity spacing, spatially uniform
+    hr::F    = 1.0              # bedrock bump height, spatially uniform
+
+    # Numerical domain
+    lx::F = 12.0       # domain size
+    ly::F = 10.0
+    nx::I = 64         # number of grids
+    ny::I = 32
+    dx::F = lx/nx      # grid size
+    dy::F = ly/ny
+    xc::Lin = LinRange(dx/2, lx-dx/2, nx) # vector of x-coordinates
+    yc::Lin = LinRange(dy/2, ly-dy/2, ny) # vector of y-coordinates
+
+    # Field parameters (defined on every grid point)
+    zs::Arr = 1/lx * xc * ones(ny)' # surface elevation
+    zb::Arr = zeros(nx, ny) # bed elevation
+    m::Arr  = zeros(nx-2, ny-2)  # source term
+    ub::Arr = zeros(nx, ny) # basal sliding speed
+
+    # Dimensionless numbers
+    Σ::F   = 1e2
+    Γ::F   = 1e5
+    Λ::F   = 0.5
+    r_ρ::F = 1000 / 910 # ρw / ρi
+
+    # Physical time stepping
+    ttot::F   = 2.0        # total simulation time
+    dt::F     = 0.1        # physical time step
+
+    # Pseudo-time iteration
+    tol::F    = 1e-6       # tolerance
+    itMax::I  = 10^3        # max number of iterations
+    damp::F   = 1-41/nx    # damping (this is a tuning parameter, dependent on e.g. grid resolution) # TODO: define how?
+    dτ::F     = (1.0/(dx^2/k/2.1) + 1.0/dt)^-1 # pseudo-time step; TODO: define how?
+end
+
+
+"""
+Allocates zeros()
+"""
+function array_allocation(nu::Para)
+    @unpack nx, ny = nu
+    qx     = zeros(nx-1, ny-1)
+    qy     = zeros(nx-1, ny-1)
+    dϕ_dτ  = zeros(nx-2, ny-2)
+    dh_dτ  = zeros(nx-2, ny-2)
+    Res_ϕ  = zeros(nx-2, ny-2)
+    Res_h  = zeros(nx-2, ny-2)
+    return qx, qy, dϕ_dτ, dh_dτ, Res_ϕ, Res_h
+end
+
+
+"""
+Calculates discharge
+"""
+function calc_q(h, dϕ_du, p::Para) # u can be x or y
+    @unpack k, α, β = p
+    q = - k .* h.^α .* abs.(dϕ_du).^(β-2) .* dϕ_du
     return q
 end
+
+"""
+Calculates water pressure
+"""
+function calc_pw(ϕ, p::Para)
+    @unpack ρw, g, zb = p
+    return ϕ .- ρw .* g .* zb
+end
+
+"""
+Calculates effective pressure
+"""
+function calc_N(ϕ, p::Para)
+    @unpack ρi, g, zs, zb = p
+    pw = calc_pw(ϕ, p)
+    return ρi * g * (zs .- zb) .- pw
+end
+
+"""
+Calculates closure rate
+"""
+function calc_vc(ϕ, h, p::Para)
+    @unpack n, A = p
+    N = calc_N(ϕ, p)
+    return 2 / n^n * A * h .* abs.(N).^(n-1) .* N
+end
+
+"""
+Calculates opening rate
+"""
+function calc_vo(h, p::Para)
+    @unpack ub, hr, lr = p
+    return ub .* (hr .- h) ./ lr
+end
+
+
+
+
 
 end # module
